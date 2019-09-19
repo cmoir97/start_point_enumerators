32abd90101c3e51f66d4567f988193e4f5ba8aab
Cameron Moir <cameronjmoir@gmail.com>
2019-09-19 13:47:48 +0100

    printed name of selected planet

--- star_system.rb
+++ star_system.rb
@@ -7,7 +7,11 @@ class StarSystem
   end
 
   def planet_names
-  p  @planets.map { |planet| planet.name}
+    p  @planets.map { |planet| planet.name}
+  end
+
+  def get_planet_by_name(name)
+    p  @planets.find { |planet| planet.name == name}
   end
 
 end
